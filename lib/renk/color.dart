24a08c8b9b9713bf941d686dbd9e23a81cb4ba46
EmircanAktay <121422518+EmircanAktay@users.noreply.github.com>
2023-01-05 14:12:56 +0300

    Add files via upload

---
+++ lib/renk/color.dart
@@ -0,0 +1,8 @@
+//Şimdi yansıtmamız gereken rengi ekleyelim
+import 'package:flutter/material.dart';
+
+class MainColor {
+  static Color primaryColor = Color(0xFF39298a);
+  static Color secondaryColor = Color(0xFF231954);
+  static Color accentColor = Color(0xFF4169e8);
+}
